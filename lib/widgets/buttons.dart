50bd6771e548e63f69f5075081c03ca52d0262c7
olasunkanmi beckley <beckleysunkanmi@gmail.com>
2024-09-02 17:59:53 +0100

    created the filled button and text button

--- lib/widgets/buttons.dart
+++ lib/widgets/buttons.dart
@@ -44,7 +44,7 @@ class CustomWideButton extends StatelessWidget {
                   Text(
                     label,
                     style: Theme.of(context).textTheme.labelLarge?.copyWith(
-                        color: context.watch<UIColors>().onPrimaryContainer
+                      color: context.watch<UIColors>().onPrimaryContainer
                     ),
                   ),
                   trailing!
@@ -59,10 +59,79 @@ class CustomWideButton extends StatelessWidget {
 }
 
 class CustomFilledButton extends StatelessWidget {
-  const CustomFilledButton({super.key});
+  final String label;
+  final VoidCallback? onTap;
+
+  const CustomFilledButton({
+    required this.label,
+    this.onTap,
+    super.key
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(); // TODO: create filled button
+    return GestureDetector(
+      onTap: onTap,
+      child: SizedBox(
+        height: 50,
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: context.watch<UIColors>().primary,
+                borderRadius: BorderRadius.circular(25)
+              ),
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: Text(
+                    label,
+                    style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                      color: context.watch<UIColors>().onPrimaryContainer
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomTextButton extends StatelessWidget {
+  final String label;
+  final VoidCallback? onTap;
+  final Color? color;
+
+  const CustomTextButton({
+    required this.label,
+    this.onTap,
+    this.color,
+    super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: SizedBox(
+        height: 40,
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            child: Text(
+              label,
+              style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                color: color ?? Theme.of(context).colorScheme.onSurface,
+                fontWeight: FontWeight.bold
+              )
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
