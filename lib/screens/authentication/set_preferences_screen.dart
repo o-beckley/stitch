a8e0b304d5263bdefa46b370285518da628507d7
olasunkanmi beckley <beckleysunkanmi@gmail.com>
2024-09-03 11:52:46 +0100

    created part of the set_preferences_screen.dart ui

--- lib/screens/authentication/set_preferences_screen.dart
+++ lib/screens/authentication/set_preferences_screen.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:stitch/config/route_paths.dart';
+import 'package:stitch/widgets/app_bar.dart';
+import 'package:stitch/widgets/buttons.dart';
+import 'package:stitch/widgets/picker.dart';
+import 'package:go_router/go_router.dart';
+import 'package:stitch/utils/router_utils.dart';
 
 class SetPreferencesScreen extends StatefulWidget {
   const SetPreferencesScreen({super.key});
@@ -8,11 +15,61 @@ class SetPreferencesScreen extends StatefulWidget {
 }
 
 class _SetPreferencesScreenState extends State<SetPreferencesScreen> {
+  final genders = ['Men', 'Women'];
+  int selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('select gender and age'),
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 0.05.sw),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView(
+                shrinkWrap: true,
+                children: [
+                  const CustomAppBar(hasBackButton: false),
+                  Text(
+                    'Tell us about yourself',
+                    style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  0.1.sw.verticalSpace,
+                  Text(
+                    'Who do you shop for?',
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
+                  0.05.sw.verticalSpace,
+                  Picker(
+                    items: genders,
+                    startingIndex: selectedIndex,
+                    onItemPicked: (index){
+                      selectedIndex = index;
+                    },
+                  ),
+                  0.1.sw.verticalSpace,
+                  Text(
+                    'How old are you?',
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
+                  0.05.sw.verticalSpace,
+                  // TODO: age picker
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 0.05.sw),
+              child: CustomWideButton(
+                label: 'Finish',
+                onTap: (){
+                  // TODO: upload preference then:
+                  GoRouter.of(context).clearStackAndNavigate(RoutePaths.home);
+                },
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
