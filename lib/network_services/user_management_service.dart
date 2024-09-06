414f7d7105435e5c0d367bcc8f2ba3c0b1582f42
olasunkanmi beckley <beckleysunkanmi@gmail.com>
2024-09-06 18:20:13 +0100

    Created the user management service

---
+++ lib/network_services/user_management_service.dart
@@ -0,0 +1,84 @@
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:stitch/models/user_model.dart';
+
+class UserManagementService extends ChangeNotifier{
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  User? get user => FirebaseAuth.instance.currentUser;
+  bool get isSignedIn => user != null;
+
+  CollectionReference<StitchUser> get _userReference => _firestore
+    .collection('users')
+    .withConverter(
+      fromFirestore: (value, options){
+        var v = value.data()!;
+        v.addAll({'id': value.id});
+        return StitchUser.fromMap(v);
+      },
+      toFirestore: (value, options){
+        var v = value.toMap();
+        v.remove('id');
+        return v;
+      }
+  );
+
+  Future<bool> get hasProfile async {
+    try{
+      if(isSignedIn){
+        final snapshot = await _userReference.doc(user!.uid).get();
+        return snapshot.exists;
+      }
+      else {
+        return false;
+      }
+    }
+    catch(e){
+      log(e.toString());
+      return false;
+    }
+  }
+
+  Future<bool> updateProfile({
+    String? firstName,
+    String? lastName,
+    String? phoneNumber,
+    String? email,
+    Gender? gender,
+    AgeGroup? ageGroup,
+    String? address,
+    List<String>? favourites,
+    List<String>? cart
+  }) async {
+    try{
+      if(isSignedIn){
+        final snapshot = await _userReference.doc(user!.uid).get();
+        final oldProfile = snapshot.data() ?? StitchUser(id: user!.uid);
+        final newProfile = oldProfile.copyWith(
+          firstName: firstName,
+          lastName: lastName,
+          phoneNumber: phoneNumber,
+          email: email,
+          gender: gender,
+          ageGroup: ageGroup,
+          address: address,
+          favourites: favourites,
+          cart: cart,
+        );
+        await _userReference.doc(user!.uid).set(newProfile);
+        log('profile updated');
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
+    catch(e){
+      log(e.toString());
+      return false;
+    }
+  }
+}
