4dab27d68fa917cfb61a541db0a8d450806ffb92
olasunkanmi beckley <beckleysunkanmi@gmail.com>
2024-09-06 18:23:06 +0100

    added email and password auth methods

--- lib/network_services/auth_service.dart
+++ lib/network_services/auth_service.dart
@@ -1,17 +1,47 @@
 import 'dart:developer';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthService extends ChangeNotifier{
-  User? get user => FirebaseAuth.instance.currentUser;
-  bool get isSignedIm => user != null;
-
   final GoogleSignIn _googleSignIn = GoogleSignIn();
 
-  Future<bool> signInWithEmailAndPassword(String email, String password) async {
-    return false;
+  User? get user => FirebaseAuth.instance.currentUser;
+  bool get isSignedIn => user != null;
+
+  Future<bool> signIn(String email, String password) async {
+    try{
+      final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
+      if(credential.user != null){
+        log('signed in');
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
+    catch(e){
+      log(e.toString());
+      return false;
+    }
+  }
+
+  Future<bool> createAccount(String email, String password) async {
+    try{
+      final credential = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password);
+      if(credential.user != null){
+        log('account created');
+        FirebaseAuth.instance.currentUser!.sendEmailVerification();
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
+    catch(e){
+      log(e.toString());
+      return false;
+    }
   }
 
   Future<bool> signInWithGoogle() async {
@@ -46,6 +76,17 @@ class AuthService extends ChangeNotifier{
     catch (e){
       log(e.toString());
       return false;
+    }
+  }
+
+  Future<bool> sendPasswordResetEmail(String email) async {
+    try {
+      await FirebaseAuth.instance.sendPasswordResetEmail(email: email);
+      return true;
+    }
+    catch(e){
+      log(e.toString());
+      return true;
     }
   }
 
