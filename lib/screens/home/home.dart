cd74cd7830dc128444d5457fdca44798f9927ab1
olasunkanmi beckley <beckleysunkanmi@gmail.com>
2024-09-08 20:43:53 +0100

    increased the opacity of the icons for the bottom navigation bar to 100% so that using a different color filter with BlendMode.srcIn would give full opacity.

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:stitch/config/asset_paths.dart';
 import 'package:stitch/network_services/auth_service.dart';
+import 'package:stitch/widgets/bottom_navigation_bar.dart';
+import 'package:stitch/widgets/buttons.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -13,13 +16,29 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: GestureDetector(
-          onTap: (){
-            context.read<AuthService>().signOut();
-          },
-          child: const Text('Home')
-        ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Center(
+              child: CustomFilledButton(
+                label: 'Sign out',
+                onTap: (){
+                  context.read<AuthService>().signOut();
+                },
+              ),
+            ),
+          ),
+          CustomBottomNavigationBar(
+            iconPaths: [
+              AssetPaths.homeIcon,
+              AssetPaths.notificationIcon,
+              AssetPaths.ordersIcon,
+              AssetPaths.profileIcon,
+            ],
+          )
+        ],
       ),
     );
   }
