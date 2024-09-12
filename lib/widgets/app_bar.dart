2faeef7f7eb8589fde8e1e2c0f81c566dfbddd84
olasunkanmi beckley <beckleysunkanmi@gmail.com>
2024-09-12 08:38:19 +0100

    Minor changes

--- lib/widgets/app_bar.dart
+++ lib/widgets/app_bar.dart
@@ -4,7 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:stitch/config/asset_paths.dart';
 import 'package:go_router/go_router.dart';
+import 'package:stitch/theme/color_theme.dart';
 import 'package:stitch/widgets/buttons.dart';
+import 'package:provider/provider.dart';
 
 class CustomAppBar extends StatelessWidget {
   final bool hasBackButton;
@@ -19,44 +21,47 @@ class CustomAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: math.max(80, 0.25.sw),
-      child: Stack(
-        children: [
-          hasBackButton
-          ? Align(
-            alignment: Alignment.centerLeft,
-            child: CustomSvgIconButton(
-              svgIconPath: AssetPaths.arrowBackIcon,
-              onTap: (){
-                if(context.canPop()){
-                  context.pop();
-                }
-              },
+    return Container(
+      color: context.watch<UIColors>().surface,
+      child: SizedBox(
+        height: math.max(80, 0.25.sw),
+        child: Stack(
+          children: [
+            hasBackButton
+            ? Align(
+              alignment: Alignment.centerLeft,
+              child: CustomSvgIconButton(
+                svgIconPath: AssetPaths.arrowBackIcon,
+                onTap: (){
+                  if(context.canPop()){
+                    context.pop();
+                  }
+                },
+              )
             )
-          )
-          : const SizedBox.shrink(),
+            : const SizedBox.shrink(),
 
-          title != null
-          ? Align(
-            alignment: Alignment.center,
-            child: Text(
-                title!,
-                style: Theme.of(context).textTheme.titleMedium,
-            ),
-          )
-          : const SizedBox.shrink(),
+            title != null
+            ? Align(
+              alignment: Alignment.center,
+              child: Text(
+                  title!,
+                  style: Theme.of(context).textTheme.titleMedium,
+              ),
+            )
+            : const SizedBox.shrink(),
 
-          actions != null
-          ? Align(
-            alignment: Alignment.centerRight,
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: actions!,
-            ),
-          )
-          : const SizedBox.shrink()
-        ],
+            actions != null
+            ? Align(
+              alignment: Alignment.centerRight,
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: actions!,
+              ),
+            )
+            : const SizedBox.shrink()
+          ],
+        ),
       ),
     );
   }
@@ -65,16 +70,19 @@ class CustomAppBar extends StatelessWidget {
 class CustomSliverAppBar extends SliverPersistentHeaderDelegate {
   final String? title;
   final bool hasBackButton;
+  final List<Widget>? actions;
+
   CustomSliverAppBar({
     this.title,
-    this.hasBackButton = true
+    this.hasBackButton = true,
+    this.actions,
   });
 
   @override
-  double get maxExtent => 0.15.sw;
+  double get maxExtent => math.max(80, 0.25.sw);
 
   @override
-  double get minExtent => 0.15.sw;
+  double get minExtent => math.max(80, 0.25.sw);
 
   @override
   bool shouldRebuild(CustomSliverAppBar oldDelegate) {
@@ -87,6 +95,7 @@ class CustomSliverAppBar extends SliverPersistentHeaderDelegate {
     return CustomAppBar(
       title: title,
       hasBackButton: hasBackButton,
+      actions: actions,
     );
   }
 }
