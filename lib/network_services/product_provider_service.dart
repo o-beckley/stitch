cb0253d21b00f141aa987a357cbd10d2b507277e
olasunkanmi beckley <beckleysunkanmi@gmail.com>
2024-09-25 14:40:46 +0100

    Created a product provider

---
+++ lib/network_services/product_provider_service.dart
@@ -0,0 +1,41 @@
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:stitch/models/seller_model.dart';
+
+class ProductProviderService extends ChangeNotifier{
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  User? get user => FirebaseAuth.instance.currentUser;
+  bool get isSignedIn => user != null;
+
+  CollectionReference<StitchSeller> get _sellerReference => _firestore
+      .collection('sellers')
+      .withConverter(
+      fromFirestore: (value, options){
+        var v = value.data()!;
+        v.addAll({'id': value.id});
+        return StitchSeller.fromMap(v);
+      },
+      toFirestore: (value, options){
+        var v = value.toMap();
+        v.remove('id');
+        return v;
+      }
+  );
+
+  Future<StitchSeller?> getSeller(String id) async {
+    try{
+      if(isSignedIn){
+        final snapshot = await _sellerReference.doc(id).get();
+        return snapshot.data();
+      }
+    }
+    catch(e){
+      log("ProductProvider.getSeller: ${e.toString()}");
+    }
+    return null;
+  }
+}
