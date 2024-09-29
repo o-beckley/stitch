0916ff29185471191ed1470a388251c139656b2d
olasunkanmi beckley <beckleysunkanmi@gmail.com>
2024-09-29 19:22:57 +0100

    added the getProduct method

--- lib/network_services/product_provider_service.dart
+++ lib/network_services/product_provider_service.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:stitch/models/product_model.dart';
 import 'package:stitch/models/seller_model.dart';
 
 class ProductProviderService extends ChangeNotifier{
@@ -25,6 +26,20 @@ class ProductProviderService extends ChangeNotifier{
         return v;
       }
   );
+  CollectionReference<Product> get _productReference => _firestore
+      .collection('products')
+      .withConverter(
+      fromFirestore: (value, options){
+        var v = value.data()!;
+        v.addAll({'id': value.id});
+        return Product.fromMap(v);
+      },
+      toFirestore: (value, options){
+        var v = value.toMap();
+        v.remove('id');
+        return v;
+      }
+  );
 
   Future<StitchSeller?> getSeller(String id) async {
     try{
@@ -35,6 +50,19 @@ class ProductProviderService extends ChangeNotifier{
     }
     catch(e){
       log("getSeller: ${e.toString()}");
+    }
+    return null;
+  }
+
+  Future<Product?> getProduct(String id) async {
+    try{
+      if(isSignedIn){
+        final snapshot = await _productReference.doc(id).get();
+        return snapshot.data();
+      }
+    }
+    catch(e){
+      log("getProduct: ${e.toString()}");
     }
     return null;
   }
