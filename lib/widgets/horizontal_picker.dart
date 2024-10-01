9c76781d9fbcf4744059d5aeb90df2904f4a33a8
olasunkanmi beckley <beckleysunkanmi@gmail.com>
2024-10-01 12:43:20 +0100

    Added an optional endPadding.

--- lib/widgets/horizontal_picker.dart
+++ lib/widgets/horizontal_picker.dart
@@ -9,11 +9,13 @@ class HorizontalPicker extends StatefulWidget {
   final List<String> items;
   final Function(int)? onItemPicked;
   final int startingIndex;
+  final double? endPadding;
 
   const HorizontalPicker({
     required this.items,
     this.onItemPicked,
     this.startingIndex = 0,
+    this.endPadding,
     super.key
   });
 
@@ -38,62 +40,66 @@ class _HorizontalPickerState extends State<HorizontalPicker> {
       child: SingleChildScrollView(
         scrollDirection: Axis.horizontal,
         child: Row(
-          children: List.generate(
-            widget.items.length,
-            (index){
-              return  Padding(
-                padding: EdgeInsets.only(
-                  left: index == 0 ? 0 : radius / 2,
-                  right: index == widget.items.length ? radius / 2 : 0
-                ),
-                child: GestureDetector(
-                  onTap: (){
-                    setState(() {
-                      selectedIndex = index;
-                    });
-                    widget.onItemPicked?.call(index);
-                  },
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Container(
-                        constraints: BoxConstraints(
-                          /// This 0.9 comes from the fact that the main screens
-                          ///  where this is used has a 0.05 padding on both sides
-                          /// 1 - 2 * 0.05 = 0.9
-                          /// and the radius / 2 comes from the inner paddings
-                          /// above the consequence of this is that it centralizes
-                          /// the contents when there are only few items,
-                          /// say 2 or 3 😉
-                          /// ... no one is gonna read this tho 😐
-                          minWidth: (0.9.sw / widget.items.length) - radius / 2
-                        ),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(radius),
-                          color: index == selectedIndex
-                          ? context.watch<UIColors>().primary
-                          : context.watch<UIColors>().surfaceContainer
-                        ),
-                        child: Padding(
-                          padding: EdgeInsets.symmetric(horizontal: radius),
-                          child: Center(
-                            child: Text(
-                              widget.items[index],
-                              style: Theme.of(context).textTheme.labelLarge?.copyWith(
-                                color: index == selectedIndex
-                                ? context.watch<UIColors>().onPrimaryContainer
-                                : context.watch<UIColors>().onSurface
+          children: [
+            widget.endPadding?.horizontalSpace ?? const SizedBox.shrink(),
+            ...List.generate(
+              widget.items.length,
+              (index){
+                return  Padding(
+                  padding: EdgeInsets.only(
+                    left: index == 0 ? 0 : radius / 2,
+                    right: index == widget.items.length ? radius / 2 : 0
+                  ),
+                  child: GestureDetector(
+                    onTap: (){
+                      setState(() {
+                        selectedIndex = index;
+                      });
+                      widget.onItemPicked?.call(index);
+                    },
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Container(
+                          constraints: BoxConstraints(
+                            /// This 0.9 comes from the fact that the main screens
+                            ///  where this is used has a 0.05 padding on both sides
+                            /// 1 - 2 * 0.05 = 0.9
+                            /// and the radius / 2 comes from the inner paddings
+                            /// above the consequence of this is that it centralizes
+                            /// the contents when there are only few items,
+                            /// say 2 or 3 😉
+                            /// ... no one is gonna read this tho 😐
+                            minWidth: (0.9.sw / widget.items.length) - radius / 2
+                          ),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(radius),
+                            color: index == selectedIndex
+                            ? context.watch<UIColors>().primary
+                            : context.watch<UIColors>().surfaceContainer
+                          ),
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(horizontal: radius),
+                            child: Center(
+                              child: Text(
+                                widget.items[index],
+                                style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                                  color: index == selectedIndex
+                                  ? context.watch<UIColors>().onPrimaryContainer
+                                  : context.watch<UIColors>().onSurface
+                                ),
                               ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              );
-            }
-          )
+                );
+              }
+            ),
+            widget.endPadding?.horizontalSpace ?? const SizedBox.shrink(),
+          ]
         ),
       ),
     );
