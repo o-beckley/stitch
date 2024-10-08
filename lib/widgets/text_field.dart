b13c8260b28182b1d6a9f814dedddd1fe913d450
olasunkanmi beckley <beckleysunkanmi@gmail.com>
2024-10-08 14:43:24 +0100

    added a max lines and max length property to the text field

--- lib/widgets/text_field.dart
+++ lib/widgets/text_field.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:stitch/theme/color_theme.dart';
 import 'package:provider/provider.dart';
 
@@ -10,6 +11,8 @@ class CustomTextField extends StatelessWidget {
   final VoidCallback? onEditingComplete;
   final FormFieldValidator<String>? validator;
   final bool obscureText;
+  final int maxLines;
+  final int? maxLength;
 
   const CustomTextField({
     required this.hintText,
@@ -19,39 +22,64 @@ class CustomTextField extends StatelessWidget {
     this.onEditingComplete,
     this.obscureText = false,
     this.validator,
+    this.maxLines = 1,
+    this.maxLength,
     super.key
   });
 
+  Widget? _counter(
+    BuildContext context,
+    {
+      required int currentLength,
+      required int? maxLength,
+      required bool isFocused,
+    }
+    ) {
+    if(isFocused && maxLength != null){
+      return Text(
+        '$currentLength / $maxLength',
+        semanticsLabel: 'character count',
+        style: Theme.of(context).textTheme.labelSmall?.copyWith(
+          color: context.watch<UIColors>().outline
+        ),
+      );
+    }
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 60,
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(5),
-            color: context.watch<UIColors>().surfaceContainer
+    return Container(
+      constraints: BoxConstraints(
+        minHeight: 60
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(5),
+        color: context.watch<UIColors>().surfaceContainer
+      ),
+      child: TextFormField(
+        controller: controller,
+        focusNode: focusNode,
+        obscureText: obscureText,
+        maxLines: maxLines,
+        maxLength: maxLength,
+        buildCounter: _counter,
+        decoration: InputDecoration(
+          hintText: hintText,
+          hintStyle: Theme.of(context).textTheme.labelLarge?.copyWith(
+            color: context.watch<UIColors>().outline
           ),
-          child: TextFormField(
-            controller: controller,
-            focusNode: focusNode,
-            obscureText: obscureText,
-            decoration: InputDecoration(
-              hintText: hintText,
-              hintStyle: Theme.of(context).textTheme.labelLarge?.copyWith(
-                color: context.watch<UIColors>().outline
-              ),
-              border: InputBorder.none,
-              contentPadding: const EdgeInsets.only(
-                left: 15,
-                right: 15,
-                top: 12
-              ),
-            ),
-            onEditingComplete: onEditingComplete,
-            onTapOutside: onTapOutside ?? (_) => FocusScope.of(context).requestFocus(FocusNode()),
-            validator: validator,
+          border: InputBorder.none,
+          contentPadding: const EdgeInsets.only(
+            left: 15,
+            right: 15,
+            top: 12
           ),
-        )
+        ),
+        onEditingComplete: onEditingComplete,
+        onTapOutside: onTapOutside ?? (_) => FocusScope.of(context).requestFocus(FocusNode()),
+        validator: validator,
+      ),
     );
   }
 }
