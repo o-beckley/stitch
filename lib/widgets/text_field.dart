41d9598f7c3a4207c44481e80f54d1eda5ec4b2f
olasunkanmi beckley <beckleysunkanmi@gmail.com>
2024-10-08 15:41:31 +0100

    clean up

--- lib/widgets/text_field.dart
+++ lib/widgets/text_field.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:stitch/theme/color_theme.dart';
 import 'package:provider/provider.dart';
 
