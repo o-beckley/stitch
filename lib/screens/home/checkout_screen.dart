8f3a83098037de3497b258d96a796bc9d042ce79
olasunkanmi beckley <beckleysunkanmi@gmail.com>
2024-10-14 15:15:01 +0100

    Added the proper delivery details to the checkout screen

--- lib/screens/home/checkout_screen.dart
+++ lib/screens/home/checkout_screen.dart
@@ -11,11 +11,13 @@ import 'package:stitch/network_services/product_provider_service.dart';
 import 'package:stitch/network_services/user_management_service.dart';
 import 'package:stitch/theme/color_theme.dart';
 import 'package:stitch/utils/toasts.dart';
+import 'package:stitch/utils/validate.dart';
 import 'package:stitch/widgets/app_bar.dart';
 import 'package:stitch/widgets/buttons.dart';
 import 'package:stitch/widgets/custom_tile.dart';
 import 'package:stitch/widgets/loading_indicator.dart';
 import 'package:stitch/widgets/placeholders.dart';
+import 'package:stitch/widgets/text_field.dart';
 
 class CheckoutScreen extends StatefulWidget {
   final List<OrderItem> orderItems;
@@ -31,8 +33,10 @@ class CheckoutScreen extends StatefulWidget {
 
 class _CheckoutScreenState extends State<CheckoutScreen> {
   bool checkingOut = false;
+  GlobalKey<FormState> phoneNumberFormKey = GlobalKey();
   late Future<double?> subtotal;
   late Future<double?> totalDeliveryFees;
+  late TextEditingController phoneNumberController;
   Address? selectedAddress;
 
   @override
@@ -40,6 +44,15 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
     super.initState();
     subtotal = getSubtotal(widget.orderItems);
     totalDeliveryFees = getDeliveryFees(widget.orderItems);
+    phoneNumberController = TextEditingController(
+      text: context.read<UserManagementService>().currentUser?.phoneNumber
+    );
+  }
+
+  @override
+  void dispose(){
+    phoneNumberController.dispose();
+    super.dispose();
   }
 
   @override
@@ -58,14 +71,68 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
                         title: 'Checkout'
                     ),
                   ),
+                  if(selectedAddress != null)
+                  SliverToBoxAdapter(
+                    child: CustomTile(
+                      title: "${selectedAddress!.street}, ${selectedAddress!.city}",
+                      subtitle: "${selectedAddress!.state}, ${selectedAddress!.country}",
+                      trailing: CustomSvgIconButton(svgIconPath: AssetPaths.arrowDownIcon),
+                      onTap: (){
+                      showModalBottomSheet(
+                          context: context,
+                          isScrollControlled: true,
+                          shape: RoundedRectangleBorder(),
+                          builder: (context){
+                            return _AddressList(
+                              onAddressSelected: (address){
+                                setState(() {
+                                  selectedAddress = address;
+                                });
+                              }
+                            );
+                          }
+                        );
+                      },
+                    )
+                  )
+                  else
                   SliverToBoxAdapter(
                     child: CustomTile(
                       hintText: "Delivery address",
                       title: "Select a delivery address",
                       trailing: CustomSvgIconButton(svgIconPath: AssetPaths.arrowDownIcon),
                       onTap: (){
-                        // TODO: show a modal bottom sheet to pick an address or add a new one
+                        showModalBottomSheet(
+                          context: context,
+                          isScrollControlled: true,
+                          shape: RoundedRectangleBorder(),
+                          builder: (context){
+                            return _AddressList(
+                              onAddressSelected: (address){
+                                setState(() {
+                                  selectedAddress = address;
+                                });
+                              }
+                            );
+                          }
+                        );
                       },
+                    ),
+                  ),
+                  SliverToBoxAdapter(child: 0.05.sw.verticalSpace),
+                  SliverToBoxAdapter(
+                    child: Form(
+                      key: phoneNumberFormKey,
+                      child: CustomTextField(
+                        hintText: "Phone number",
+                        controller: phoneNumberController,
+                        validator: (value){
+                          if(!Validate.phoneNumber(value)){
+                            return "Enter a valid phone number";
+                          }
+                          return null;
+                        },
+                      ),
                     ),
                   ),
                   SliverToBoxAdapter(child: 0.05.sw.verticalSpace),
@@ -103,32 +170,7 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
                                   color: context.watch<UIColors>().onPrimaryContainer.withOpacity(0.5)
                                 ),
                               ),
-                            onTap: () async {
-                              setState(() {
-                                checkingOut = true;
-                              });
-                              final bool successful = await context.read<UserManagementService>().checkout(
-                                orderItems: widget.orderItems,
-                                deliveryDetails: DeliveryDetails( // TODO
-                                  address: Address(
-                                    street: 'street',
-                                    city: 'city',
-                                    state: 'state',
-                                    country: 'country'
-                                  ),
-                                  phoneNumber: "09053221941"
-                                ),
-                              );
-                              setState(() {
-                                checkingOut = false;
-                              });
-                              if(successful && context.mounted){
-                                context.push(RoutePaths.orderSuccessScreen);
-                              }
-                              else if (context.mounted){
-                                Toasts.showToast("Something went wrong, please try again later.", context);
-                              }
-                            }
+                            onTap: _placeOrder
                           ),
                         );
                       }
@@ -147,6 +189,33 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
         )
       )
     );
+  }
+
+  void _placeOrder() async {
+    if(selectedAddress != null && phoneNumberFormKey.currentState!.validate()){
+      setState(() {
+        checkingOut = true;
+      });
+      final bool successful = await context.read<UserManagementService>().checkout(
+        orderItems: widget.orderItems,
+        deliveryDetails: DeliveryDetails(
+          address: selectedAddress!,
+          phoneNumber: phoneNumberController.text
+        ),
+      );
+      setState(() {
+        checkingOut = false;
+      });
+      if(successful && mounted){
+        context.push(RoutePaths.orderSuccessScreen);
+      }
+      else if (mounted){
+        Toasts.showToast("Something went wrong, please try again later.", context);
+      }
+    }
+    else{
+      Toasts.showToast("select a payment method and delivery details", context);
+    }
   }
 
   Widget priceWidgetThingy(){
@@ -246,5 +315,43 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
   Future<double?> getDeliveryFees(List<OrderItem> items) async{
     // TODO: group by sellers first then get individual shipping fees of the sellers
     return 0;
+  }
+}
+
+
+class _AddressList extends StatelessWidget {
+  final ValueChanged<Address> onAddressSelected;
+
+  const _AddressList({
+    required this.onAddressSelected,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final addresses = context.read<UserManagementService>().currentUser?.addresses;
+    if(addresses != null && addresses.isNotEmpty){
+      return Padding(
+        padding: EdgeInsets.all(0.05.sw),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: List.generate(
+            addresses.length,
+            (index){
+              return CustomTile(
+                title: "${addresses[index].street}, ${addresses[index].city}",
+                subtitle: "${addresses[index].state}, ${addresses[index].country}",
+                onTap: (){
+                  onAddressSelected(addresses[index]);
+                  if(context.canPop()){
+                    context.pop();
+                  }
+                },
+              );
+            }
+          )
+        ),
+      );
+    }
+    return const Placeholder();
   }
 }
