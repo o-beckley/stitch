d8971269baba4154189071bedba5b3e2c38ceb26
olasunkanmi beckley <beckleysunkanmi@gmail.com>
2024-10-14 15:18:04 +0100

    Minor change

--- lib/screens/authentication/create_account_screen.dart
+++ lib/screens/authentication/create_account_screen.dart
@@ -5,6 +5,7 @@ import 'package:stitch/network_services/auth_service.dart';
 import 'package:stitch/network_services/user_management_service.dart';
 import 'package:stitch/theme/color_theme.dart';
 import 'package:stitch/utils/router_utils.dart';
+import 'package:stitch/utils/validate.dart';
 import 'package:stitch/widgets/app_bar.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:stitch/widgets/buttons.dart';
@@ -85,9 +86,7 @@ class _CreateAccountScreenState extends State<CreateAccountScreen> {
                 focusNode: emailFocus,
                 onEditingComplete: () => FocusScope.of(context).requestFocus(passwordFocus),
                 validator: (value){
-                  String pattern = r"""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])""";
-                  RegExp regex = RegExp(pattern);
-                  if (value == null || !regex.hasMatch(value)){
+                  if (value == null || !Validate.email(value)){
                     return 'Enter a valid email';
                   }
                   return null;
